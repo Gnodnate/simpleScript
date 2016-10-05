16363557c92e12e4d1352d433e2a099abd585aba
Daniel Tan <julykitt@gmail.com>
2016-10-05 17:17:23 +0800

    the final path in the same path of script

--- ffmpeg-android/build_android.sh
+++ ffmpeg-android/build_android.sh
@@ -1,48 +1,50 @@
 #!/bin/sh
 
-cd `dirname $0` #change the work path into script file path
+NDK=~/sdk/android-sdk-linux/ndk-bundle # NDK path
+platformVersion=android-21 # platform version
+
+scriptPath=`dirname $0`
+cd $scriptPath #change the work path into script file path
 
 #prepare final folder
 LIBS=$(pwd)/android/libs
-#INCLUDE=$LIBS/include
 
-NDK=~/sdk/android-sdk-linux/ndk-bundle
+cd ffmpeg/
 
 #for cpu in armeabi arm64-v8a x86 x86_64; do
-for cpu in x86_64; do
+for cpu in armeabi; do
+    ADDI_CFLAGS=""  # only need for arm
     case "$cpu" in
         armeabi)
             CPU=armeabi
             ARCH=arm
-            SYSROOT=$NDK/platforms/android-21/arch-arm/
-            BIN=`echo $NDK/toolchains/$ARCH-*/prebuilt/*/bin/`
+            SYSROOT=$NDK/platforms/$platformVersion/arch-arm/
+	    BIN=`echo $NDK/toolchains/$ARCH-*/prebuilt/*/bin/`
             CROSS_PREFIX=$BIN/arm-linux-androideabi-
-        ;;
+	    ADDI_CFLAGS="-marm"
+            ;;
         arm64-v8a)
-            SYSROOT=$NDK/platforms/android-21/arch-arm64/
+            SYSROOT=$NDK/platforms/$platformVersion/arch-arm64/
             CPU=arm64-v8a
             ARCH=aarch64
-            BIN=`echo $NDK/toolchains/$ARCH-*/prebuilt/*/bin/`
+	    BIN=`echo $NDK/toolchains/$ARCH-*/prebuilt/*/bin/`
             CROSS_PREFIX=$BIN/aarch64-linux-android-
-        ;;
+            ;;
         x86)
-            SYSROOT=$NDK/platforms/android-21/arch-x86/
+            SYSROOT=$NDK/platforms/$platformVersion/arch-x86/
             CPU=x86
             ARCH=x86
-            BIN=`echo $NDK/toolchains/$ARCH-*/prebuilt/*/bin/`
+	    BIN=`echo $NDK/toolchains/$ARCH-*/prebuilt/*/bin/`
             CROSS_PREFIX=$BIN/i686-linux-android-
-        ;;
+            ;;
         x86_64)
-            SYSROOT=$NDK/platforms/android-21/arch-x86_64/
+            SYSROOT=$NDK/platforms/$platformVersion/arch-x86_64/
             CPU=x86_64
             ARCH=x86_64
-            BIN=`echo $NDK/toolchains/$ARCH-*/prebuilt/*/bin/`
+	    BIN=`echo $NDK/toolchains/$ARCH-*/prebuilt/*/bin/`
             CROSS_PREFIX=$BIN/x86_64-linux-android-
-        ;;
+            ;;
     esac
-
-    # clean last build, if any.
-    make clean
 
     echo "================================================"
     echo "=================build for $CPU=============="
@@ -55,10 +57,6 @@ for cpu in x86_64; do
         --disable-static \
         --disable-doc \
         --disable-programs \
-        --disable-ffmpeg \
-        --disable-ffplay \
-        --disable-ffprobe \
-        --disable-ffserver \
         --disable-avdevice \
         --disable-doc \
         --disable-symver \
@@ -66,13 +64,14 @@ for cpu in x86_64; do
         --target-os=linux \
         --arch=$ARCH \
         --enable-cross-compile \
+	--extra-cflags="-Os -fpic $ADDI_CFLAGS" \
         --sysroot=$SYSROOT 
     if [ $? -eq 0 ]; then
         make clean
     	cpuNum=4
     	buildplatform=`uname -s`
   	if [ $buildplatform = "Linux" ]; then
-		cupNum=`grep 'processor' /proc/cpuinfo |sort|uniq|wc -l`
+	    cupNum=`grep 'processor' /proc/cpuinfo |sort|uniq|wc -l`
 	fi
         make -j$cpuNum install
         if [ $? -eq 0 ]; then
@@ -80,6 +79,10 @@ for cpu in x86_64; do
             cp -r $PREFIX/include $LIBS/$CPU/
             mkdir -p $LIBS/$CPU
             cp -r $PREFIX/lib/* $LIBS/$CPU
+	    # clean for next build
+	    make clean
+	else
+	    exit
         fi
     fi
 done
