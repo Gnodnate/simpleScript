8d0dfab92a9cde355ee44fdf4605e3d7fd1197c6
Odie <julykitt@gmail.com>
2017-07-18 14:38:17 +0800

    remove test code, and add mulit-thread support

--- mergeMP4/mergeMP4.sh
+++ mergeMP4/mergeMP4.sh
@@ -3,12 +3,13 @@
 outFile="output.mp4"
 if [ -n $2 ]; then
     outFile=$2
-    if [[ ! $file =~ ".mp4" ]]; then
+    if [[ ! $outFile =~ ".mp4" ]]; then
         outFile=$2".mp4"
     fi
 fi
 echo "\033[7;30m Files will convert to "$outFile"\033[0m"
-exit 0
+
+threadNum=$(grep 'processor' /proc/cpuinfo | sort -u | wc -l)
 
 tsString=""
 if [ -d $1 ] ; then
@@ -21,13 +22,13 @@ if [ -d $1 ] ; then
             else
                 tsString=$tsString"|"$tsFile
             fi
-            ffmpeg -i $file -c copy -bsf:v h264_mp4toannexb -f mpegts $tsFile
+            ffmpeg -threads $threadNum -i $file -c copy -bsf:v h264_mp4toannexb -f mpegts $tsFile
         fi
     done
     if [ -z $tsString ]; then 
-        ffmpeg -i "concat:"$tsString -c copy -bsf:a aac_adtstoasc $1/$outFile
-        rm -fr /tmp/*.ts
+	echo "No MP4 file need to be converted"
     else
-        echo "No MP4 file need to be converted"
+        ffmpeg -threads $threadNum -i "concat:"$tsString -c copy -bsf:a aac_adtstoasc $1/$outFile
+        rm -fr /tmp/*.ts
     fi
 fi
